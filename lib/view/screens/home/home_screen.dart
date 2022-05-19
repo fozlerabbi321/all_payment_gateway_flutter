c460d40fe1574ee27f40e16ffb22af27e6ad17bf
fozlerabbi321 <fozlerabbiinfo@gmail.com>
2022-05-19 18:14:40 +0600

    Added razor payment gateway

--- lib/view/screens/home/home_screen.dart
+++ lib/view/screens/home/home_screen.dart
@@ -1,19 +1,62 @@
 import 'dart:developer';
-
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_paypal/flutter_paypal.dart';
+import 'package:flutter_sslcommerz/model/SSLCAdditionalInitializer.dart';
+import 'package:flutter_sslcommerz/model/SSLCCustomerInfoInitializer.dart';
+import 'package:flutter_sslcommerz/model/SSLCEMITransactionInitializer.dart';
+import 'package:flutter_sslcommerz/model/SSLCSdkType.dart';
+import 'package:flutter_sslcommerz/model/SSLCShipmentInfoInitializer.dart';
+import 'package:flutter_sslcommerz/model/SSLCTransactionInfoModel.dart';
+import 'package:flutter_sslcommerz/model/SSLCommerzInitialization.dart';
+import 'package:flutter_sslcommerz/model/SSLCurrencyType.dart';
+import 'package:flutter_sslcommerz/model/sslproductinitilizer/General.dart';
+import 'package:flutter_sslcommerz/model/sslproductinitilizer/SSLCProductInitializer.dart';
+import 'package:flutter_sslcommerz/sslcommerz.dart';
 import 'package:get/get.dart';
 import 'package:mystarter/constants/strings.dart';
 import 'package:mystarter/constants/style_data.dart';
 import 'package:mystarter/view/screens/gateway/flutter_wave_screen.dart';
+import 'package:razorpay_flutter/razorpay_flutter.dart';
 
 import '../../../constants/size_config.dart';
 import '../../../helper/stripe_payment_helpter.dart';
 import '../../widgets/default_btn.dart';
 import '../gateway/paystack_screen.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  //ssl commerz
+  dynamic formData = {};
+
+  //razorpay
+  late Razorpay _razorpay;
+
+  //end razorpay
+
+  @override
+  void initState() {
+    super.initState();
+    //razorpay
+    _razorpay = Razorpay();
+    _razorpay.on(Razorpay.EVENT_PAYMENT_SUCCESS, _handlePaymentSuccess);
+    _razorpay.on(Razorpay.EVENT_PAYMENT_ERROR, _handlePaymentError);
+    _razorpay.on(Razorpay.EVENT_EXTERNAL_WALLET, _handleExternalWallet);
+    //end razorpay
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    //razorpay
+    _razorpay.clear();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -61,8 +104,7 @@ class HomeScreen extends StatelessWidget {
                 radius: 10.0,
                 title: 'Flutterwave'.tr,
                 onPress: () async {
-                 Get.to(() => const FlutterWaveScreen());
-
+                  Get.to(() => const FlutterWaveScreen());
                 },
               ),
               kHeightBox10,
@@ -75,6 +117,21 @@ class HomeScreen extends StatelessWidget {
                 },
               ),
               kHeightBox10,
+              DefaultBtn(
+                width: SizeConfig.screenWidth,
+                radius: 10.0,
+                title: 'Razorpay'.tr,
+                onPress: openCheckout,
+              ),
+              kHeightBox10,
+              DefaultBtn(
+                width: SizeConfig.screenWidth,
+                radius: 10.0,
+                title: 'SSL Commerz'.tr,
+                onPress: () async {
+                  await sslCommercePayment(context);
+                },
+              ),
             ],
           ),
         ),
@@ -82,7 +139,7 @@ class HomeScreen extends StatelessWidget {
     );
   }
 
-  Future<void> handlePaypalPayment(BuildContext context)async {
+  Future<void> handlePaypalPayment(BuildContext context) async {
     Navigator.of(context).push(
       MaterialPageRoute(
         builder: (BuildContext context) => UsePaypal(
@@ -102,12 +159,7 @@ class HomeScreen extends StatelessWidget {
                   "shipping_discount": 0
                 }
               },
-              "description":
-              "The payment transaction description.",
-              // "payment_options": {
-              //   "allowed_payment_method":
-              //       "INSTANT_FUNDING_SOURCE"
-              // },
+              "description": "The payment transaction description.",
               "item_list": {
                 "items": [
                   {
@@ -144,6 +196,142 @@ class HomeScreen extends StatelessWidget {
           },
         ),
       ),
+    );
+  }
+
+  Future<void> sslCommercePayment(BuildContext context) async {
+    Sslcommerz sslCommerce = Sslcommerz(
+      initializer: SSLCommerzInitialization(
+        multi_card_name: formData['multicard'],
+        currency: SSLCurrencyType.BDT,
+        product_category: "Test",
+
+        ///SSL SandBox ///
+        ipn_url: "www.ipnurl.com",
+        sdkType: SSLCSdkType.TESTBOX,
+        store_id: 'testi6119f4717446f',
+        store_passwd: 'testi6119f4717446f@ssl',
+
+        ///SSL SandBox End///
+        total_amount: 150.00,
+        tran_id: DateTime.now().microsecondsSinceEpoch.toString(),
+      ),
+    );
+    sslCommerce
+        .addEMITransactionInitializer(
+          sslcemiTransactionInitializer: SSLCEMITransactionInitializer(
+            emi_options: 1,
+            emi_max_list_options: 3,
+            emi_selected_inst: 2,
+          ),
+        )
+        .addShipmentInfoInitializer(
+          sslcShipmentInfoInitializer: SSLCShipmentInfoInitializer(
+            shipmentMethod: "yes",
+            numOfItems: 10,
+            shipmentDetails: ShipmentDetails(
+              shipAddress1: 'Uttara-7,Dhaka',
+              shipCity: 'Dhaka',
+              shipCountry: 'Bangladesh',
+              shipName: 'your ship name',
+              shipPostCode: '1230',
+            ),
+          ),
+        )
+        .addCustomerInfoInitializer(
+          customerInfoInitializer: SSLCCustomerInfoInitializer(
+            customerName: kName,
+            customerEmail: kEmail,
+            customerAddress1: 'uttaha,dhaka-1230',
+            customerState: '',
+            customerCity: '',
+            customerPostCode: '1230',
+            customerCountry: 'Bangladesh',
+            customerPhone: kPhone,
+          ),
+        )
+        .addProductInitializer(
+          sslcProductInitializer: SSLCProductInitializer(
+            productName: "Gadgets",
+            productCategory: "Widgets",
+            general: General(
+              general: "General Purpose",
+              productProfile: "Product Profile",
+            ),
+          ),
+        )
+        .addAdditionalInitializer(
+          sslcAdditionalInitializer: SSLCAdditionalInitializer(
+            valueA: "app",
+            valueB: "value b",
+            valueC: "value c",
+            valueD: "value d",
+          ),
+        );
+    var result = await sslCommerce.payNow();
+    log('ssl Result ====>' + result.toString());
+    if (result is PlatformException) {
+      log("the response is: " +
+          result.message.toString() +
+          " code: " +
+          result.code.toString());
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(result.message ?? ''),
+        ),
+      );
+    } else {
+      SSLCTransactionInfoModel model = result;
+      log("ssL json" + model.toJson().toString());
+      if (model.aPIConnect == 'DONE' && model.status == 'VALID') {
+        showCustomSnackBar('Payment success', isError: false);
+      } else {
+        showCustomSnackBar('Payment Failed');
+      }
+    }
+  }
+
+  ///razorpay
+  void openCheckout() async {
+    var options = {
+      'key': 'rzp_live_ILgsfZCZoFIKMb',
+      'amount': 250,
+      'name': kName,
+      'description': 'RTX 3090ti',
+      'retry': {
+        'enabled': true,
+        'max_count': 1,
+      },
+      'send_sms_hash': true,
+      'prefill': {
+        'contact': kPhone,
+        'email': kEmail,
+      },
+      'external': {
+        'wallets': ['paytm']
+      }
+    };
+
+    try {
+      _razorpay.open(options);
+    } catch (e) {
+      debugPrint('Error: e');
+    }
+  }
+
+  void _handlePaymentSuccess(PaymentSuccessResponse response) {
+    showCustomSnackBar("SUCCESS: " + response.paymentId!, isError: false);
+  }
+
+  void _handlePaymentError(PaymentFailureResponse response) {
+    showCustomSnackBar(
+      "ERROR: " + response.code.toString() + " - " + response.message!,
+    );
+  }
+
+  void _handleExternalWallet(ExternalWalletResponse response) {
+    showCustomSnackBar(
+      "EXTERNAL_WALLET: " + response.walletName!,
     );
   }
 }
